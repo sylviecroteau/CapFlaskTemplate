acabf0939063980fa47a481958370e1cc765c3ca
Sylvie Croteau <s_sylvie.croteau@ousd.org>
2022-03-02 09:51:47 -0800

    First website steps

--- app/routes/default.py
+++ app/routes/default.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
