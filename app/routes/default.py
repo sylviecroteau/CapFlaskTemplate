0446a7640bc810a6d4956867e7fdc2378aa221a4
conchesness <37635142+conchesness@users.noreply.github.com>
2022-03-07 09:31:58 -0800

    comments for data and routes

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,6 +6,6 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
