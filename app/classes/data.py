b09bec5b28a11d63088bcdea510a58f3fa08e303
conchesness <37635142+conchesness@users.noreply.github.com>
2022-03-11 13:30:54 -0800

    minor comments

--- app/classes/data.py
+++ app/classes/data.py
@@ -1,7 +1,8 @@
-# This is where all the database tables are defined. Each class name is the name of the data collection
-# and each item is a data 'field' that stores a piece of data.  Data fields have types
-# like IntField, StringField etc.  This uses the Mongoengine Python Library. When you interact with the 
-# data you are creating an onject that is an instance of the class.
+# This is where all the database collections are defined. A collection is a place to hold a defined 
+# set of data like Users, Posts, Comments. Collections are defined below as classes. Each class name is 
+# the name of the data collection and each item is a data 'field' that stores a piece of data.  Data 
+# fields have types like IntField, StringField etc.  This uses the Mongoengine Python Library. When 
+# you interact with the data you are creating an onject that is an instance of the class.
 
 from app import app
 from flask import flash
@@ -12,7 +13,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import datetime as dt
 import jwt
 from time import time
-from bson.objectid import ObjectId
+#from bson.objectid import ObjectId
 
 class User(UserMixin, Document):
     username = StringField()
